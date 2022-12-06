c7da5c2ae36f292af18ebfc1d3b7a95f37c276be
Haneesh <56464193+saihaneesh26@users.noreply.github.com>
2022-12-06 22:43:41 +0530

    Update prgm3.c

--- test_codes/prgm3.c
+++ test_codes/prgm3.c
@@ -4,15 +4,15 @@
 
 //iterns
 void main(){
-    int iterns,i,itern=1;
+    int iterns,i,itern[8];
     printf("enter iterns:");
     scanf("%d",&iterns);
     //ref: http://jakascorner.com/blog/2016/06/omp-for-scheduling.html
     #pragma omp parallel for schedule(static,2)
     for(i=1; i<=iterns; i++){
         int t = omp_get_thread_num();
-        itern+=1;
-        itern%=2;
-        printf("thread %d itern %d value: %d\n",t,itern,i);
+        itern[t]+=1;
+        printf("thread %d itern %d value: %d\n",t,itern[t],i);
+        
     }
 }
