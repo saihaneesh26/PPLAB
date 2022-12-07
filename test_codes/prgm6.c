ee5a558e3a9539139fc871bd10bd02340f5393b6
Himanshu <76443309+himanshu-dxm@users.noreply.github.com>
2022-12-07 09:40:58 +0530

    Corrected Prg6

--- test_codes/prgm6.c
+++ test_codes/prgm6.c
@@ -3,6 +3,15 @@
 #include<omp.h>
 //compile using : gcc -fopenmp prgm6.c vect_addn_func.c
 //to run ./a.out in ubuntu and a in windows
+
+void vector_addn_parallel(int n,int *res_vector,int *vector_a, int *vector_b){
+    #pragma omp parallel for	
+	for(int i=0; i<n;i++)
+	{
+		res_vector[i] = vector_a[i]+vector_b[i];
+	}
+}
+
 void main(){
     int n = 100000;
 	int vector_a[100000],vector_b[100000], res_vector[100000];
