ef27859beed22cd18c7db0708ba2982ebf7e93c9
Haneesh <56464193+saihaneesh26@users.noreply.github.com>
2022-12-08 11:26:00 +0530

    Add files via upload

---
+++ fibonacci/fib_updated.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<omp.h>
+
+int fib(int n)
+{
+	int i,j;
+	if(n<2) return n;
+	else {
+		#pragma omp task shared(i) firstprivate(n)
+		i = fib(n-1);
+		#pragma omp task shared(j) firstprivate(n)
+		j = fib(n-2);
+		#pragma omp taskwait
+		return i+j;
+	}
+	
+}
+void main()
+{
+	int n = 10;	
+	omp_set_num_threads(4);
+	#pragma omp parallel shared(n)
+	{
+		#pragma omp single
+		printf("fib(%d) = %d\n",n,fib(n));
+	}	
+}
