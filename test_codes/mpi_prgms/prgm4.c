a2180a106c79b3d9dc6aadfcf27c52418627b028
Haneesh <56464193+saihaneesh26@users.noreply.github.com>
2022-12-13 18:40:21 +0530

    Create prgm4.c

---
+++ test_codes/mpi_prgms/prgm4.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<mpi.h>
+
+void main(int argc,char * argv[]){
+	int n,rank,size;
+	scanf("%d",&n);
+	MPI_Init(&argc,&argv);
+	MPI_Comm_size(MPI_COMM_WORLD,&size);
+	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+	int root=0;
+	
+	int global;	
+		//len = (n/size)+1;
+		//printf("len %d\n",len);
+	
+	MPI_Bcast(&n,1,MPI_INT,root,MPI_COMM_WORLD);
+	int localprod=1;
+	int len = (n/size)+1;
+	//printf("RANK %d\n",rank);
+	for(int i=1;i<=len;i++){
+		printf("rank %d len:%d i:%d\n n:%d",rank,len,i,n);
+		int val = (rank*len) + i;
+		printf("rank: %d val :%d\n",rank,val);
+		if(val<=n)
+		localprod *= val;
+		else break;
+	}
+	printf("LOCAL PROF:%d\n",localprod);
+MPI_Reduce(&localprod,&global,1,MPI_INT,MPI_PROD,root,MPI_COMM_WORLD);
+if(rank==root)
+		printf("Factorial of %d is %d\n",n,global);
+	MPI_Finalize();
+
+	
+}
