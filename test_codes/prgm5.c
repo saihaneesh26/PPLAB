a170666cb7eddbeeaba9c8401021ea8be9b06200
Haneesh <56464193+saihaneesh26@users.noreply.github.com>
2023-01-16 06:30:57 +0530

    Changed to primes in 1-n

--- test_codes/prgm5.c
+++ test_codes/prgm5.c
@@ -1,43 +1,37 @@
-//first n primes
+//primes in 1-n
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<omp.h>
 
-//primes
-int isprime(int n){
-    for(int i=2;i<=n/2;i++){
-        if(n%i==0)
-            return 0;
+int isPrime(int n){
+  if(n == 0 || n == 1){
+    return 0;
+  }
+  for(int i = 2; i <= n/2; i++){
+    if(n%i == 0){
+      return 0;
     }
-    return 1;
+  }
+  return 1;
 }
 
-void main(){
-    int i=0,n=1000,x=2,primes[1100];
-    double start = omp_get_wtime();
-    #pragma omp parallel
-    while(i<n){
-        if(isprime(x)){
-            primes[i] = x;
-            #pragma omp atomic 
-            i++;
-        }
-        #pragma omp atomic
-        x++;
-    }
-    double end = omp_get_wtime();
-    printf("parallel exec time: %f\n",end-start);
+int main(){
 
-    i=0;x=2;
-    start =omp_get_wtime();
-    while(i<n){
-        if(isprime(x)){
-            primes[i] = x;
-            i++;
-        }
-        x++;
-    }
-    end = omp_get_wtime();
-    printf("serial exec time: %f\n",end-start);
+  int x = 1,n = 100;
+  int primes[100];
 
+  double start = omp_get_wtime();
+  #pragma omp parallel for
+  for(int i = 1; i <= n;i++){
+    if(isPrime(i)){
+      int t = omp_get_thread_num();
+      printf("process : %d , %d \n",t,i);
+    }
+  }
+  double end = omp_get_wtime();
+  printf("The time taken is %lf ",end - start);
+  // for(int i = 0; i < n; i++){
+    //If needed print numbers here from primes[i]
+  // }
 }
